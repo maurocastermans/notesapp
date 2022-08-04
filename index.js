563695725eedb1a7d45a69735f5873d0a2d33202
mauro <mauro.castermans@student.kuleuven.be>
2022-08-04 18:59:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,187 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const cors = require('cors')
+app.use(cors())
+
+// Phonebook
+// const morgan = require('morgan')
+
+// morgan.token('request-body', (req, res) => { 
+//   const body = req.body
+//   return JSON.stringify(body)
+// })
+
+// app.use(morgan((tokens, req, res) => {
+//   if (tokens.method(req, res) === "POST") {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, 'content-length'), '-',
+//       tokens['response-time'](req, res), 'ms',
+//       tokens['request-body'](req,res)
+//     ].join(' ')
+//   }
+// }))
+
+// let persons = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ]
+ 
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
+
+// app.get('/api/persons', (request, response) => {
+//   response.json(persons)
+// })
+
+// app.get('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     const person = persons.find(person => person.id === id)
+//     if (person) {
+//         response.json(person)
+//     } else {
+//         response.status(404).end()
+//     } 
+// })
+
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body
+//   if (!body) {
+//     return response.status(400).json({ 
+//       error: 'name or number missing' 
+//     })
+//   } else if (persons.find(p => p.name === body.name)) {
+//     return response.status(400).json({ 
+//       error: 'name must be unique' 
+//     })
+//   }
+
+//   const person = {
+//     id: Math.floor(Math.random() * 40000),
+//     name: body.name,
+//     number: body.number
+//   }
+  
+//   persons = persons.concat(person)
+
+//   response.json(person)
+//   })
+
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     persons = persons.filter(person => person.id !== id)
+  
+//     response.status(204).end()
+// })
+
+// app.get('/info', (request, response) => {
+//   const responseString = 
+//   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}`)
+
+// })
+
+// Notes
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2022-05-30T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2022-05-30T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2022-05-30T19:20:14.298Z",
+        important: true
+    }
+]
+ 
+app.get('/', (request, response) => {
+response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+  
+  notes = notes.concat(note)
+
+  response.json(note)
+  })
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
